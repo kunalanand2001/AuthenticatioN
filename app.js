346415ebdf13464b7d884ed28c076ea8735a9579
Kunal <kunalanand2001@gmail.com>
2021-12-15 12:03:46 +0530

    anomalous secret submission to public.

--- app.js
+++ app.js
@@ -33,7 +33,8 @@ mongoose.connect("mongodb://localhost:27017/userDB");
 const userSchema = new mongoose.Schema({
     email: String,
     password: String,
-    googleId: String
+    googleId: String,
+    secret:String
 });
 
 userSchema.plugin(passportLocalMongoose);
@@ -62,7 +63,7 @@ passport.use(new GoogleStrategy({
     userProfileURL: "https://www.googleapis.com/oauth2/v3/userinfo"
   },
   function(accessToken, refreshToken, profile, cb) {
-      console.log(profile);
+    //   console.log(profile);
 
     User.findOrCreate({ googleId: profile.id }, function (err, user) {
       return cb(err, user);
@@ -96,12 +97,42 @@ app.get("/register",function(req,res){
 });
 
 app.get("/secrets",function(req,res){
+    User.find({"secret":{$ne: null}}, function(err,foundUsers){
+        if(err){
+            console.log(err)
+        }else{
+            if(foundUsers){
+                res.render("secrets",{usersWithSecrets: foundUsers});
+            }
+        }
+    });
+});
+
+app.post("/submit",function(req,res){
+     const submittedSecret = req.body.secret;
+     console.log(req.user.id);
+
+     User.findById(req.user.id,function(err,foundUser){
+         if(err){
+             console.log(err);
+         }else{
+             if(foundUser){
+                 foundUser.secret = submittedSecret;
+                 foundUser.save(function(){
+                     res.redirect("/secrets");
+                 });
+             }
+         }
+     });
+});
+
+app.get("/submit",function(req,res){
     if(req.isAuthenticated()){
-        res.render("secrets");
+        res.render("submit");
     }else{
         res.redirect("/login");
     }
-});
+})
 
 app.get("/logout",function(req,res){
     req.logout();
